57fa50e2f9961e9170fa75194ff8115814849c24
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2024-04-11 15:25:00 -0400

    feat: Brought in `source-ref.sh` script to remove dependency on externally available script for this action to function

---
+++ scripts/source-ref.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# This is used to determine what to use as the base comparison point for determining what modules to deploy. The logic
+# is as follows:
+#   - If we are on the main branch, the comparison is only the current commit.
+#   - If we are not on main, the comparison is to the current state of the main branch.
+if [[ "${GITHUB_REF##*/}" == "main" ]]; then
+  git rev-parse 'HEAD^'
+else
+  git rev-parse 'origin/main'
+fi
